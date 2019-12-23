89e7e788153ab4b56db28f59e5fd9386b7b6148d
MSCULHA <pclordu@gmail.com>
2019-12-23 22:05:02 +0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+import 'InAppPurchasePage.dart';
+import 'package:friendly_fire/blocs/app_bloc.dart';
+import 'package:friendly_fire/blocs/bloc_provider.dart';
+import 'package:friendly_fire/blocs/home_screen_bloc.dart';
+import 'SplashScreen.dart';
+
+void main(){
+
+  runApp(MaterialApp(
+    title: "Flutter Dersleri",
+    routes: {
+      '/' : (context) => Home(),
+
+
+    },
+
+
+  ));
+
+}
+
+/*
+
+
+        home: BlocProvider(bloc: HomeScreenBloc(), child: Home()),
+        debugShowCheckedModeBanner: false,
+ */
